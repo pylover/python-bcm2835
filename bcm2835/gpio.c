f7f2c1e8fb4b46fe6a27ccc84d8fc06d45cb1598
pylover <vahid.mardani@gmail.com>
2015-01-10 04:45:13 +0330

    planning

--- bcm2835/gpio.c
+++ bcm2835/gpio.c
@@ -1,4 +1,12 @@
 
+##include "gpio.h"
+
+static PyMethodDef module_methods[] = {
+    {"system",  spam_system, METH_VARARGS,
+     "Execute a shell command."},
+
+    {NULL, NULL, 0, NULL}        /* Sentinel - Dummy, i think so*/
+};
 
 PyMODINIT_FUNC initgpio(void)
 {
--- bcm2835/gpio.h
+++ bcm2835/gpio.h
@@ -1,12 +1,2 @@
 #include <Python.h>
 #include "_bcm2835.h"
-
-static PyMethodDef module_methods[] = {
-    {"system",  spam_system, METH_VARARGS,
-     "Execute a shell command."},
-
-    {NULL, NULL, 0, NULL}        /* Sentinel - Dummy, i think so*/
-};
-
-
-PyMODINIT_FUNC initgpio(void);
