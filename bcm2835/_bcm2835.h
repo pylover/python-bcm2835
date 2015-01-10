d489d95780cc167ad1fe1caabc0e5b2115775618
pylover <vahid.mardani@gmail.com>
2015-01-10 04:22:39 +0330

    planning

---
+++ bcm2835/_bcm2835.h
@@ -0,0 +1,7 @@
+
+#include <stdint.h>
+
+/// This means pin HIGH, true, 3.3volts on a pin.
+#define HIGH 0x1
+/// This means pin LOW, false, 0volts on a pin.
+#define LOW  0x0
---
+++ bcm2835/gpio.h
@@ -0,0 +1,2 @@
+#include <Python.h>
+#include "_bcm2835.h"
