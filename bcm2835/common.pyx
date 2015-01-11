476a9e98be21de2e80f1ac25c56a5b664eb2572b
pylover <vahid.mardani@gmail.com>
2015-01-11 15:42:39 +0330

    planning

--- bcm2835/common.pyx
+++ bcm2835/common.pyx
@@ -1,12 +1,58 @@
 
 cdef extern from "_bcm2835.h" nogil:
+  # This means pin HIGH, true, 3.3volts on a pin.
   int c_HIGH "HIGH"
+
+  # This means pin LOW, false, 0volts on a pin.
   int c_LOW "LOW"
+
+  # Speed of the core clock core_clk
+  int c_BCM2835_CORE_CLK_HZ "BCM2835_CORE_CLK_HZ"
+
+  # Physical addresses for various peripheral register sets
+  # Base Physical Address of the BCM 2835 peripheral registers
+  int c_BCM2835_PERI_BASE "BCM2835_PERI_BASE"
+
+  # Base Physical Address of the System Timer registers
+  int c_BCM2835_ST_BASE "BCM2835_ST_BASE"
+
+  # Base Physical Address of the Pads registers
+  int c_BCM2835_GPIO_PADS "BCM2835_GPIO_PADS"
+
+  # Base Physical Address of the Clock/timer registers
+  int c_BCM2835_CLOCK_BASE "BCM2835_CLOCK_BASE"
+
+  # Base Physical Address of the GPIO registers
+  int c_BCM2835_GPIO_BASE "BCM2835_GPIO_BASE"
+
+  # Base Physical Address of the SPI0 registers
+  int c_BCM2835_SPI0_BASE "BCM2835_SPI0_BASE"
+
+  # Base Physical Address of the BSC0 registers
+  int c_BCM2835_BSC0_BASE "BCM2835_BSC0_BASE"
+
+  # Base Physical Address of the PWM registers
+  int c_BCM2835_GPIO_PWM "BCM2835_GPIO_PWM"
+
+  # Base Physical Address of the BSC1 registers
+  int c_BCM2835_BSC1_BASE "BCM2835_BSC1_BASE"
+
+
   int c_bcm2835_init "bcm2835_init"()
 
 
 HIGH = c_HIGH
 LOW = c_LOW
+CORE_CLK_HZ = c_BCM2835_CORE_CLK_HZ
+PERI_BASE = c_BCM2835_PERI_BASE
+ST_BASE = c_BCM2835_ST_BASE
+GPIO_PADS = c_BCM2835_GPIO_PADS
+CLOCK_BASE = c_BCM2835_CLOCK_BASE
+GPIO_BASE = c_BCM2835_GPIO_BASE
+SPI0_BASE = c_BCM2835_SPI0_BASE
+BSC0_BASE = c_BCM2835_BSC0_BASE
+GPIO_PWM = c_BCM2835_GPIO_PWM
+BSC1_BASE = c_BCM2835_BSC1_BASE
 
 def init():
   return c_bcm2835_init()
@@ -14,6 +60,15 @@ def init():
 __all__ = [
   'HIGH',
   'LOW',
-
+  'CORE_CLK_HZ',
+  'PERI_BASE',
+  'ST_BASE',
+  'GPIO_PADS',
+  'CLOCK_BASE',
+  'GPIO_BASE',
+  'SPI0_BASE',
+  'BSC0_BASE',
+  'GPIO_PWM',
+  'BSC1_BASE',
   'init'
 ]
--- bcm2835/tests/test_common.py
+++ bcm2835/tests/test_common.py
@@ -10,6 +10,18 @@ class TestCommon(unittest.TestCase):
     self.assertEqual(LOW, 0x0)
     self.assertEqual(HIGH, 0x1)
 
+    self.assertEqual(CORE_CLK_HZ, 250000000)
+    self.assertEqual(PERI_BASE, 0x20000000)
+    self.assertEqual(ST_BASE, PERI_BASE + 0x3000)
+    self.assertEqual(GPIO_PADS, PERI_BASE + 0x100000)
+    self.assertEqual(CLOCK_BASE, PERI_BASE + 0x101000)
+    self.assertEqual(GPIO_BASE, PERI_BASE + 0x200000)
+    self.assertEqual(SPI0_BASE, PERI_BASE + 0x204000)
+    self.assertEqual(BSC0_BASE, PERI_BASE + 0x205000)
+    self.assertEqual(GPIO_PWM, PERI_BASE + 0x20C000)
+    self.assertEqual(BSC1_BASE, PERI_BASE + 0x804000)
+
+
 
 if __name__ == '__main__':
   unittest.main()
