0aaaeeb2e43ec8f40653ffc8246b6898df6a3003
pylover <vahid.mardani@gmail.com>
2015-01-11 22:23:39 +0330

    All enums and unit tests

--- bcm2835/bcm2835.pyx
+++ bcm2835/bcm2835.pyx
@@ -61,81 +61,6 @@ GPPUDCLK1 = c_BCM2835_GPPUDCLK1   # GPIO Pin Pull-up/down Enable Clock 1
 
 
 """
-Port function select modes for bcm2835_gpio_fsel()
-"""
-GPIO_FSEL_INPT = c_BCM2835_GPIO_FSEL_INPT  # Input
-GPIO_FSEL_OUTP = c_BCM2835_GPIO_FSEL_OUTP  # Output
-GPIO_FSEL_ALT0 = c_BCM2835_GPIO_FSEL_ALT0  # Alternate function 0
-GPIO_FSEL_ALT1 = c_BCM2835_GPIO_FSEL_ALT1  # Alternate function 1
-GPIO_FSEL_ALT2 = c_BCM2835_GPIO_FSEL_ALT2  # Alternate function 2
-GPIO_FSEL_ALT3 = c_BCM2835_GPIO_FSEL_ALT3  # Alternate function 3
-GPIO_FSEL_ALT4 = c_BCM2835_GPIO_FSEL_ALT4  # Alternate function 4
-GPIO_FSEL_ALT5 = c_BCM2835_GPIO_FSEL_ALT5  # Alternate function 5
-GPIO_FSEL_MASK = c_BCM2835_GPIO_FSEL_MASK  # Function select bits mask
-
-
-"""
-Pullup/Pulldown defines for bcm2835_gpio_pud()
-"""
-GPIO_PUD_OFF = c_BCM2835_GPIO_PUD_OFF  # Off ? disable pull-up/down
-GPIO_PUD_DOWN = c_BCM2835_GPIO_PUD_DOWN  # Enable Pull Down control
-GPIO_PUD_UP = c_BCM2835_GPIO_PUD_UP  # Enable Pull Up control
-
-
-"""
-Here we define Raspberry Pin GPIO pins on P1 in terms of the underlying BCM GPIO pin numbers.
-These can be passed as a pin number to any function requiring a pin.
-Not all pins on the RPi 26 bin IDE plug are connected to GPIO pins
-and some can adopt an alternate function.
-RPi version 2 has some slightly different pinouts, and these are values RPI_V2_*.
-At bootup, pins 8 and 10 are set to UART0_TXD, UART0_RXD (ie the alt0 function) respectively
-When SPI0 is in use (ie after bcm2835_spi_begin()), pins 19, 21, 23, 24, 26 are dedicated to SPI
-and cant be controlled independently
-"""
-# RPI Version 1
-RPI_GPIO_P1_03 = c_RPI_GPIO_P1_03  # Version 1, Pin P1-03
-RPI_GPIO_P1_05 = c_RPI_GPIO_P1_05  # Version 1, Pin P1-05
-RPI_GPIO_P1_07 = c_RPI_GPIO_P1_07  # Version 1, Pin P1-07
-RPI_GPIO_P1_08 = c_RPI_GPIO_P1_08  # Version 1, Pin P1-08, defaults to alt function 0 UART0_TXD
-RPI_GPIO_P1_10 = c_RPI_GPIO_P1_10  # Version 1, Pin P1-10, defaults to alt function 0 UART0_RXD
-RPI_GPIO_P1_11 = c_RPI_GPIO_P1_11  # Version 1, Pin P1-11
-RPI_GPIO_P1_12 = c_RPI_GPIO_P1_12  # Version 1, Pin P1-12, can be PWM channel 0 in ALT FUN 5
-RPI_GPIO_P1_13 = c_RPI_GPIO_P1_13  # Version 1, Pin P1-13
-RPI_GPIO_P1_15 = c_RPI_GPIO_P1_15  # Version 1, Pin P1-15
-RPI_GPIO_P1_16 = c_RPI_GPIO_P1_16  # Version 1, Pin P1-16
-RPI_GPIO_P1_18 = c_RPI_GPIO_P1_18  # Version 1, Pin P1-18
-RPI_GPIO_P1_19 = c_RPI_GPIO_P1_19  # Version 1, Pin P1-19, MOSI when SPI0 in use
-RPI_GPIO_P1_21 = c_RPI_GPIO_P1_21  # Version 1, Pin P1-21, MISO when SPI0 in use
-RPI_GPIO_P1_22 = c_RPI_GPIO_P1_22  # Version 1, Pin P1-22
-RPI_GPIO_P1_23 = c_RPI_GPIO_P1_23  # Version 1, Pin P1-23, CLK when SPI0 in use
-RPI_GPIO_P1_24 = c_RPI_GPIO_P1_24  # Version 1, Pin P1-24, CE0 when SPI0 in use
-RPI_GPIO_P1_26 = c_RPI_GPIO_P1_26  # Version 1, Pin P1-26, CE1 when SPI0 in use
-
-# RPI Version 2
-RPI_V2_GPIO_P1_03 = c_RPI_V2_GPIO_P1_03  # Version 2, Pin P1-03
-RPI_V2_GPIO_P1_05 = c_RPI_V2_GPIO_P1_05  # Version 2, Pin P1-05
-RPI_V2_GPIO_P1_07 = c_RPI_V2_GPIO_P1_07  # Version 2, Pin P1-07
-RPI_V2_GPIO_P1_08 = c_RPI_V2_GPIO_P1_08  # Version 2, Pin P1-08, defaults to alt function 0 UART0_TXD
-RPI_V2_GPIO_P1_10 = c_RPI_V2_GPIO_P1_10  # Version 2, Pin P1-10, defaults to alt function 0 UART0_RXD
-RPI_V2_GPIO_P1_11 = c_RPI_V2_GPIO_P1_11  # Version 2, Pin P1-11
-RPI_V2_GPIO_P1_12 = c_RPI_V2_GPIO_P1_12  # Version 2, Pin P1-12, can be PWM channel 0 in ALT FUN 5
-RPI_V2_GPIO_P1_13 = c_RPI_V2_GPIO_P1_13  # Version 2, Pin P1-13
-RPI_V2_GPIO_P1_15 = c_RPI_V2_GPIO_P1_15  # Version 2, Pin P1-15
-RPI_V2_GPIO_P1_16 = c_RPI_V2_GPIO_P1_16  # Version 2, Pin P1-16
-RPI_V2_GPIO_P1_18 = c_RPI_V2_GPIO_P1_18  # Version 2, Pin P1-18
-RPI_V2_GPIO_P1_19 = c_RPI_V2_GPIO_P1_19  # Version 2, Pin P1-19, MOSI when SPI0 in use
-RPI_V2_GPIO_P1_21 = c_RPI_V2_GPIO_P1_21  # Version 2, Pin P1-21, MISO when SPI0 in use
-RPI_V2_GPIO_P1_22 = c_RPI_V2_GPIO_P1_22  # Version 2, Pin P1-22
-RPI_V2_GPIO_P1_23 = c_RPI_V2_GPIO_P1_23  # Version 2, Pin P1-23, CLK when SPI0 in use
-RPI_V2_GPIO_P1_24 = c_RPI_V2_GPIO_P1_24  # Version 2, Pin P1-24, CE0 when SPI0 in use
-RPI_V2_GPIO_P1_26 = c_RPI_V2_GPIO_P1_26  # Version 2, Pin P1-26, CE1 when SPI0 in use
-RPI_V2_GPIO_P5_03 = c_RPI_V2_GPIO_P5_03  # Version 2, Pin P5-03
-RPI_V2_GPIO_P5_04 = c_RPI_V2_GPIO_P5_04  # Version 2, Pin P5-04
-RPI_V2_GPIO_P5_05 = c_RPI_V2_GPIO_P5_05  # Version 2, Pin P5-05
-RPI_V2_GPIO_P5_06 = c_RPI_V2_GPIO_P5_06  # Version 2, Pin P5-06
-
-
-"""
 Defines for SPI
 GPIO register offsets from BCM2835_SPI0_BASE.
 Offsets into the SPI Peripheral block in bytes per 10.5 SPI Register Map
@@ -222,6 +147,7 @@ BSC_S_DONE = c_BCM2835_BSC_S_DONE  # Transfer DONE
 BSC_S_TA = c_BCM2835_BSC_S_TA  # Transfer Active
 BSC_FIFO_SIZE = c_BCM2835_BSC_FIFO_SIZE  # BSC FIFO size
 
+
 """
 Defines for ST
 GPIO register offsets from BCM2835_ST_BASE.
@@ -275,6 +201,174 @@ PWM0_REPEATFF = c_BCM2835_PWM0_REPEATFF  # Repeat last value if FIFO empty
 PWM0_SERIAL = c_BCM2835_PWM0_SERIAL  # Run in serial mode
 PWM0_ENABLE = c_BCM2835_PWM0_ENABLE  # Channel Enable
 
+## Enums
+
+"""
+Port function select modes for bcm2835_gpio_fsel()
+"""
+GPIO_FSEL_INPT = c_BCM2835_GPIO_FSEL_INPT  # Input
+GPIO_FSEL_OUTP = c_BCM2835_GPIO_FSEL_OUTP  # Output
+GPIO_FSEL_ALT0 = c_BCM2835_GPIO_FSEL_ALT0  # Alternate function 0
+GPIO_FSEL_ALT1 = c_BCM2835_GPIO_FSEL_ALT1  # Alternate function 1
+GPIO_FSEL_ALT2 = c_BCM2835_GPIO_FSEL_ALT2  # Alternate function 2
+GPIO_FSEL_ALT3 = c_BCM2835_GPIO_FSEL_ALT3  # Alternate function 3
+GPIO_FSEL_ALT4 = c_BCM2835_GPIO_FSEL_ALT4  # Alternate function 4
+GPIO_FSEL_ALT5 = c_BCM2835_GPIO_FSEL_ALT5  # Alternate function 5
+GPIO_FSEL_MASK = c_BCM2835_GPIO_FSEL_MASK  # Function select bits mask
+
+
+"""
+Pullup/Pulldown defines for bcm2835_gpio_pud()
+"""
+GPIO_PUD_OFF = c_BCM2835_GPIO_PUD_OFF  # Off ? disable pull-up/down
+GPIO_PUD_DOWN = c_BCM2835_GPIO_PUD_DOWN  # Enable Pull Down control
+GPIO_PUD_UP = c_BCM2835_GPIO_PUD_UP  # Enable Pull Up control
+
+
+"""
+Here we define Raspberry Pin GPIO pins on P1 in terms of the underlying BCM GPIO pin numbers.
+These can be passed as a pin number to any function requiring a pin.
+Not all pins on the RPi 26 bin IDE plug are connected to GPIO pins
+and some can adopt an alternate function.
+RPi version 2 has some slightly different pinouts, and these are values RPI_V2_*.
+At bootup, pins 8 and 10 are set to UART0_TXD, UART0_RXD (ie the alt0 function) respectively
+When SPI0 is in use (ie after bcm2835_spi_begin()), pins 19, 21, 23, 24, 26 are dedicated to SPI
+and cant be controlled independently
+"""
+# RPI Version 1
+RPI_GPIO_P1_03 = c_RPI_GPIO_P1_03  # Version 1, Pin P1-03
+RPI_GPIO_P1_05 = c_RPI_GPIO_P1_05  # Version 1, Pin P1-05
+RPI_GPIO_P1_07 = c_RPI_GPIO_P1_07  # Version 1, Pin P1-07
+RPI_GPIO_P1_08 = c_RPI_GPIO_P1_08  # Version 1, Pin P1-08, defaults to alt function 0 UART0_TXD
+RPI_GPIO_P1_10 = c_RPI_GPIO_P1_10  # Version 1, Pin P1-10, defaults to alt function 0 UART0_RXD
+RPI_GPIO_P1_11 = c_RPI_GPIO_P1_11  # Version 1, Pin P1-11
+RPI_GPIO_P1_12 = c_RPI_GPIO_P1_12  # Version 1, Pin P1-12, can be PWM channel 0 in ALT FUN 5
+RPI_GPIO_P1_13 = c_RPI_GPIO_P1_13  # Version 1, Pin P1-13
+RPI_GPIO_P1_15 = c_RPI_GPIO_P1_15  # Version 1, Pin P1-15
+RPI_GPIO_P1_16 = c_RPI_GPIO_P1_16  # Version 1, Pin P1-16
+RPI_GPIO_P1_18 = c_RPI_GPIO_P1_18  # Version 1, Pin P1-18
+RPI_GPIO_P1_19 = c_RPI_GPIO_P1_19  # Version 1, Pin P1-19, MOSI when SPI0 in use
+RPI_GPIO_P1_21 = c_RPI_GPIO_P1_21  # Version 1, Pin P1-21, MISO when SPI0 in use
+RPI_GPIO_P1_22 = c_RPI_GPIO_P1_22  # Version 1, Pin P1-22
+RPI_GPIO_P1_23 = c_RPI_GPIO_P1_23  # Version 1, Pin P1-23, CLK when SPI0 in use
+RPI_GPIO_P1_24 = c_RPI_GPIO_P1_24  # Version 1, Pin P1-24, CE0 when SPI0 in use
+RPI_GPIO_P1_26 = c_RPI_GPIO_P1_26  # Version 1, Pin P1-26, CE1 when SPI0 in use
+
+# RPI Version 2
+RPI_V2_GPIO_P1_03 = c_RPI_V2_GPIO_P1_03  # Version 2, Pin P1-03
+RPI_V2_GPIO_P1_05 = c_RPI_V2_GPIO_P1_05  # Version 2, Pin P1-05
+RPI_V2_GPIO_P1_07 = c_RPI_V2_GPIO_P1_07  # Version 2, Pin P1-07
+RPI_V2_GPIO_P1_08 = c_RPI_V2_GPIO_P1_08  # Version 2, Pin P1-08, defaults to alt function 0 UART0_TXD
+RPI_V2_GPIO_P1_10 = c_RPI_V2_GPIO_P1_10  # Version 2, Pin P1-10, defaults to alt function 0 UART0_RXD
+RPI_V2_GPIO_P1_11 = c_RPI_V2_GPIO_P1_11  # Version 2, Pin P1-11
+RPI_V2_GPIO_P1_12 = c_RPI_V2_GPIO_P1_12  # Version 2, Pin P1-12, can be PWM channel 0 in ALT FUN 5
+RPI_V2_GPIO_P1_13 = c_RPI_V2_GPIO_P1_13  # Version 2, Pin P1-13
+RPI_V2_GPIO_P1_15 = c_RPI_V2_GPIO_P1_15  # Version 2, Pin P1-15
+RPI_V2_GPIO_P1_16 = c_RPI_V2_GPIO_P1_16  # Version 2, Pin P1-16
+RPI_V2_GPIO_P1_18 = c_RPI_V2_GPIO_P1_18  # Version 2, Pin P1-18
+RPI_V2_GPIO_P1_19 = c_RPI_V2_GPIO_P1_19  # Version 2, Pin P1-19, MOSI when SPI0 in use
+RPI_V2_GPIO_P1_21 = c_RPI_V2_GPIO_P1_21  # Version 2, Pin P1-21, MISO when SPI0 in use
+RPI_V2_GPIO_P1_22 = c_RPI_V2_GPIO_P1_22  # Version 2, Pin P1-22
+RPI_V2_GPIO_P1_23 = c_RPI_V2_GPIO_P1_23  # Version 2, Pin P1-23, CLK when SPI0 in use
+RPI_V2_GPIO_P1_24 = c_RPI_V2_GPIO_P1_24  # Version 2, Pin P1-24, CE0 when SPI0 in use
+RPI_V2_GPIO_P1_26 = c_RPI_V2_GPIO_P1_26  # Version 2, Pin P1-26, CE1 when SPI0 in use
+RPI_V2_GPIO_P5_03 = c_RPI_V2_GPIO_P5_03  # Version 2, Pin P5-03
+RPI_V2_GPIO_P5_04 = c_RPI_V2_GPIO_P5_04  # Version 2, Pin P5-04
+RPI_V2_GPIO_P5_05 = c_RPI_V2_GPIO_P5_05  # Version 2, Pin P5-05
+RPI_V2_GPIO_P5_06 = c_RPI_V2_GPIO_P5_06  # Version 2, Pin P5-06
+
+
+"""
+Specifies the SPI data bit ordering for bcm2835_spi_setBitOrder()
+"""
+SPI_BIT_ORDER_LSBFIRST = c_BCM2835_SPI_BIT_ORDER_LSBFIRST  # ///< LSB First
+SPI_BIT_ORDER_MSBFIRST = c_BCM2835_SPI_BIT_ORDER_MSBFIRST  # ///< MSB First
+
+
+"""
+Specify the SPI chip select pin(s)
+"""
+SPI_MODE0 = c_BCM2835_SPI_MODE0  # ///< CPOL = 0, CPHA = 0
+SPI_MODE1 = c_BCM2835_SPI_MODE1  # ///< CPOL = 0, CPHA = 1
+SPI_MODE2 = c_BCM2835_SPI_MODE2  # ///< CPOL = 1, CPHA = 0
+SPI_MODE3 = c_BCM2835_SPI_MODE3  # ///< CPOL = 1, CPHA = 1
+
+"""
+Specify the SPI chip select pin(s)
+"""
+SPI_CS0 = c_BCM2835_SPI_CS0  # Chip Select 0
+SPI_CS1 = c_BCM2835_SPI_CS1  # Chip Select 1
+SPI_CS2 = c_BCM2835_SPI_CS2  # Chip Select 2 (ie pins CS1 and CS2 are asserted)
+SPI_CS_NONE = c_BCM2835_SPI_CS_NONE  # No CS, control it yourself
+
+"""
+Specifies the divider used to generate the SPI clock from the system clock.
+Figures below give the divider, clock period and clock frequency.
+Clock divided is based on nominal base clock rate of 250MHz
+It is reported that (contrary to the documentation) any even divider may used.
+The frequencies shown for each divider have been confirmed by measurement
+"""
+SPI_CLOCK_DIVIDER_65536 = c_BCM2835_SPI_CLOCK_DIVIDER_65536  # ///< 65536 = 262.144us = 3.814697260kHz
+SPI_CLOCK_DIVIDER_32768 = c_BCM2835_SPI_CLOCK_DIVIDER_32768  # ///< 32768 = 131.072us = 7.629394531kHz
+SPI_CLOCK_DIVIDER_16384 = c_BCM2835_SPI_CLOCK_DIVIDER_16384  # ///< 16384 = 65.536us = 15.25878906kHz
+SPI_CLOCK_DIVIDER_8192 = c_BCM2835_SPI_CLOCK_DIVIDER_8192  # ///< 8192 = 32.768us = 30/51757813kHz
+SPI_CLOCK_DIVIDER_4096 = c_BCM2835_SPI_CLOCK_DIVIDER_4096  # ///< 4096 = 16.384us = 61.03515625kHz
+SPI_CLOCK_DIVIDER_2048 = c_BCM2835_SPI_CLOCK_DIVIDER_2048  # ///< 2048 = 8.192us = 122.0703125kHz
+SPI_CLOCK_DIVIDER_1024 = c_BCM2835_SPI_CLOCK_DIVIDER_1024  # ///< 1024 = 4.096us = 244.140625kHz
+SPI_CLOCK_DIVIDER_512 = c_BCM2835_SPI_CLOCK_DIVIDER_512  # ///< 512 = 2.048us = 488.28125kHz
+SPI_CLOCK_DIVIDER_256 = c_BCM2835_SPI_CLOCK_DIVIDER_256  # ///< 256 = 1.024us = 976.5625MHz
+SPI_CLOCK_DIVIDER_128 = c_BCM2835_SPI_CLOCK_DIVIDER_128  # ///< 128 = 512ns = = 1.953125MHz
+SPI_CLOCK_DIVIDER_64 = c_BCM2835_SPI_CLOCK_DIVIDER_64  # ///< 64 = 256ns = 3.90625MHz
+SPI_CLOCK_DIVIDER_32 = c_BCM2835_SPI_CLOCK_DIVIDER_32  # ///< 32 = 128ns = 7.8125MHz
+SPI_CLOCK_DIVIDER_16 = c_BCM2835_SPI_CLOCK_DIVIDER_16  # ///< 16 = 64ns = 15.625MHz
+SPI_CLOCK_DIVIDER_8 = c_BCM2835_SPI_CLOCK_DIVIDER_8  # ///< 8 = 32ns = 31.25MHz
+SPI_CLOCK_DIVIDER_4 = c_BCM2835_SPI_CLOCK_DIVIDER_4  # ///< 4 = 16ns = 62.5MHz
+SPI_CLOCK_DIVIDER_2 = c_BCM2835_SPI_CLOCK_DIVIDER_2  # ///< 2 = 8ns = 125MHz, fastest you can get
+SPI_CLOCK_DIVIDER_1 = c_BCM2835_SPI_CLOCK_DIVIDER_1  # ///< 1 = 262.144us = 3.814697260kHz, same as 0/65536
+
+
+"""
+Specifies the divider used to generate the I2C clock from the system clock.
+Clock divided is based on nominal base clock rate of 250MHz
+"""
+I2C_CLOCK_DIVIDER_2500 = c_BCM2835_I2C_CLOCK_DIVIDER_2500  # ///< 2500 = 10us = 100 kHz
+I2C_CLOCK_DIVIDER_626 = c_BCM2835_I2C_CLOCK_DIVIDER_626  # ///< 622 = 2.504us = 399.3610 kHz
+I2C_CLOCK_DIVIDER_150 = c_BCM2835_I2C_CLOCK_DIVIDER_150  # ///< 150 = 60ns = 1.666 MHz (default at reset)
+I2C_CLOCK_DIVIDER_148 = c_BCM2835_I2C_CLOCK_DIVIDER_148  # ///< 148 = 59ns = 1.689 MHz
+
+
+"""
+Specifies the reason codes for the bcm2835_i2c_write and bcm2835_i2c_read functions.
+"""
+I2C_REASON_OK = c_BCM2835_I2C_REASON_OK  # ///< Success
+I2C_REASON_ERROR_NACK = c_BCM2835_I2C_REASON_ERROR_NACK  # ///< Received a NACK
+I2C_REASON_ERROR_CLKT = c_BCM2835_I2C_REASON_ERROR_CLKT  # ///< Received Clock Stretch Timeout
+I2C_REASON_ERROR_DATA = c_BCM2835_I2C_REASON_ERROR_DATA  # ///< Not all data is sent / received
+
+
+"""
+Specifies the divider used to generate the PWM clock from the system clock.
+Figures below give the divider, clock period and clock frequency.
+Clock divided is based on nominal PWM base clock rate of 19.2MHz
+The frequencies shown for each divider have been confirmed by measurement
+"""
+PWM_CLOCK_DIVIDER_32768 = c_BCM2835_PWM_CLOCK_DIVIDER_32768  # ///< 32768 = 585Hz
+PWM_CLOCK_DIVIDER_16384 = c_BCM2835_PWM_CLOCK_DIVIDER_16384  # ///< 16384 = 1171.8Hz
+PWM_CLOCK_DIVIDER_8192 = c_BCM2835_PWM_CLOCK_DIVIDER_8192  # ///< 8192 = 2.34375kHz
+PWM_CLOCK_DIVIDER_4096 = c_BCM2835_PWM_CLOCK_DIVIDER_4096  # ///< 4096 = 4.6875kHz
+PWM_CLOCK_DIVIDER_2048 = c_BCM2835_PWM_CLOCK_DIVIDER_2048  # ///< 2048 = 9.375kHz
+PWM_CLOCK_DIVIDER_1024 = c_BCM2835_PWM_CLOCK_DIVIDER_1024  # ///< 1024 = 18.75kHz
+PWM_CLOCK_DIVIDER_512 = c_BCM2835_PWM_CLOCK_DIVIDER_512  # ///< 512 = 37.5kHz
+PWM_CLOCK_DIVIDER_256 = c_BCM2835_PWM_CLOCK_DIVIDER_256  # ///< 256 = 75kHz
+PWM_CLOCK_DIVIDER_128 = c_BCM2835_PWM_CLOCK_DIVIDER_128  # ///< 128 = 150kHz
+PWM_CLOCK_DIVIDER_64 = c_BCM2835_PWM_CLOCK_DIVIDER_64  # ///< 64 = 300kHz
+PWM_CLOCK_DIVIDER_32 = c_BCM2835_PWM_CLOCK_DIVIDER_32  # ///< 32 = 600.0kHz
+PWM_CLOCK_DIVIDER_16 = c_BCM2835_PWM_CLOCK_DIVIDER_16  # ///< 16 = 1.2MHz
+PWM_CLOCK_DIVIDER_8 = c_BCM2835_PWM_CLOCK_DIVIDER_8  # ///< 8 = 2.4MHz
+PWM_CLOCK_DIVIDER_4 = c_BCM2835_PWM_CLOCK_DIVIDER_4  # ///< 4 = 4.8MHz
+PWM_CLOCK_DIVIDER_2 = c_BCM2835_PWM_CLOCK_DIVIDER_2  # ///< 2 = 9.6MHz, fastest you can get
+PWM_CLOCK_DIVIDER_1 = c_BCM2835_PWM_CLOCK_DIVIDER_1  # ///< 1 = 4.6875kHz, same as divider 4096
+
 def init():
   return c_bcm2835_init()
 
@@ -323,59 +417,6 @@ __all__ = [
   'GPPUD',
   'GPPUDCLK0',
   'GPPUDCLK1',
-
-  'GPIO_FSEL_INPT',
-  'GPIO_FSEL_OUTP',
-  'GPIO_FSEL_ALT0',
-  'GPIO_FSEL_ALT1',
-  'GPIO_FSEL_ALT2',
-  'GPIO_FSEL_ALT3',
-  'GPIO_FSEL_ALT4',
-  'GPIO_FSEL_ALT5',
-  'GPIO_FSEL_MASK',
-
-  'GPIO_PUD_OFF',
-  'GPIO_PUD_DOWN',
-  'GPIO_PUD_UP',
-
-  'RPI_GPIO_P1_03',
-  'RPI_GPIO_P1_05',
-  'RPI_GPIO_P1_07',
-  'RPI_GPIO_P1_08',
-  'RPI_GPIO_P1_10',
-  'RPI_GPIO_P1_11',
-  'RPI_GPIO_P1_12',
-  'RPI_GPIO_P1_13',
-  'RPI_GPIO_P1_15',
-  'RPI_GPIO_P1_16',
-  'RPI_GPIO_P1_18',
-  'RPI_GPIO_P1_19',
-  'RPI_GPIO_P1_21',
-  'RPI_GPIO_P1_22',
-  'RPI_GPIO_P1_23',
-  'RPI_GPIO_P1_24',
-  'RPI_GPIO_P1_26',
-  'RPI_V2_GPIO_P1_03',
-  'RPI_V2_GPIO_P1_05',
-  'RPI_V2_GPIO_P1_07',
-  'RPI_V2_GPIO_P1_08',
-  'RPI_V2_GPIO_P1_10',
-  'RPI_V2_GPIO_P1_11',
-  'RPI_V2_GPIO_P1_12',
-  'RPI_V2_GPIO_P1_13',
-  'RPI_V2_GPIO_P1_15',
-  'RPI_V2_GPIO_P1_16',
-  'RPI_V2_GPIO_P1_18',
-  'RPI_V2_GPIO_P1_19',
-  'RPI_V2_GPIO_P1_21',
-  'RPI_V2_GPIO_P1_22',
-  'RPI_V2_GPIO_P1_23',
-  'RPI_V2_GPIO_P1_24',
-  'RPI_V2_GPIO_P1_26',
-  'RPI_V2_GPIO_P5_03',
-  'RPI_V2_GPIO_P5_04',
-  'RPI_V2_GPIO_P5_05',
-  'RPI_V2_GPIO_P5_06',
 
   'SPI0_CS',
   'SPI0_FIFO',
@@ -469,6 +510,117 @@ __all__ = [
   'PWM0_REPEATFF',
   'PWM0_SERIAL',
   'PWM0_ENABLE',
+
+  'GPIO_FSEL_INPT',
+  'GPIO_FSEL_OUTP',
+  'GPIO_FSEL_ALT0',
+  'GPIO_FSEL_ALT1',
+  'GPIO_FSEL_ALT2',
+  'GPIO_FSEL_ALT3',
+  'GPIO_FSEL_ALT4',
+  'GPIO_FSEL_ALT5',
+  'GPIO_FSEL_MASK',
+
+  'GPIO_PUD_OFF',
+  'GPIO_PUD_DOWN',
+  'GPIO_PUD_UP',
+
+  'RPI_GPIO_P1_03',
+  'RPI_GPIO_P1_05',
+  'RPI_GPIO_P1_07',
+  'RPI_GPIO_P1_08',
+  'RPI_GPIO_P1_10',
+  'RPI_GPIO_P1_11',
+  'RPI_GPIO_P1_12',
+  'RPI_GPIO_P1_13',
+  'RPI_GPIO_P1_15',
+  'RPI_GPIO_P1_16',
+  'RPI_GPIO_P1_18',
+  'RPI_GPIO_P1_19',
+  'RPI_GPIO_P1_21',
+  'RPI_GPIO_P1_22',
+  'RPI_GPIO_P1_23',
+  'RPI_GPIO_P1_24',
+  'RPI_GPIO_P1_26',
+  'RPI_V2_GPIO_P1_03',
+  'RPI_V2_GPIO_P1_05',
+  'RPI_V2_GPIO_P1_07',
+  'RPI_V2_GPIO_P1_08',
+  'RPI_V2_GPIO_P1_10',
+  'RPI_V2_GPIO_P1_11',
+  'RPI_V2_GPIO_P1_12',
+  'RPI_V2_GPIO_P1_13',
+  'RPI_V2_GPIO_P1_15',
+  'RPI_V2_GPIO_P1_16',
+  'RPI_V2_GPIO_P1_18',
+  'RPI_V2_GPIO_P1_19',
+  'RPI_V2_GPIO_P1_21',
+  'RPI_V2_GPIO_P1_22',
+  'RPI_V2_GPIO_P1_23',
+  'RPI_V2_GPIO_P1_24',
+  'RPI_V2_GPIO_P1_26',
+  'RPI_V2_GPIO_P5_03',
+  'RPI_V2_GPIO_P5_04',
+  'RPI_V2_GPIO_P5_05',
+  'RPI_V2_GPIO_P5_06',
+
+  "SPI_BIT_ORDER_LSBFIRST",
+  "SPI_BIT_ORDER_MSBFIRST",
+
+  "SPI_MODE0",
+  "SPI_MODE1",
+  "SPI_MODE2",
+  "SPI_MODE3",
+
+  "SPI_CS0",
+  "SPI_CS1",
+  "SPI_CS2",
+  "SPI_CS_NONE",
+
+  "SPI_CLOCK_DIVIDER_65536",
+  "SPI_CLOCK_DIVIDER_32768",
+  "SPI_CLOCK_DIVIDER_16384",
+  "SPI_CLOCK_DIVIDER_8192",
+  "SPI_CLOCK_DIVIDER_4096",
+  "SPI_CLOCK_DIVIDER_2048",
+  "SPI_CLOCK_DIVIDER_1024",
+  "SPI_CLOCK_DIVIDER_512",
+  "SPI_CLOCK_DIVIDER_256",
+  "SPI_CLOCK_DIVIDER_128",
+  "SPI_CLOCK_DIVIDER_64",
+  "SPI_CLOCK_DIVIDER_32",
+  "SPI_CLOCK_DIVIDER_16",
+  "SPI_CLOCK_DIVIDER_8",
+  "SPI_CLOCK_DIVIDER_4",
+  "SPI_CLOCK_DIVIDER_2",
+  "SPI_CLOCK_DIVIDER_1",
+
+  "I2C_CLOCK_DIVIDER_2500",
+  "I2C_CLOCK_DIVIDER_626",
+  "I2C_CLOCK_DIVIDER_150",
+  "I2C_CLOCK_DIVIDER_148",
+
+  "I2C_REASON_OK",
+  "I2C_REASON_ERROR_NACK",
+  "I2C_REASON_ERROR_CLKT",
+  "I2C_REASON_ERROR_DATA",
+
+  "PWM_CLOCK_DIVIDER_32768",
+  "PWM_CLOCK_DIVIDER_16384",
+  "PWM_CLOCK_DIVIDER_8192",
+  "PWM_CLOCK_DIVIDER_4096",
+  "PWM_CLOCK_DIVIDER_2048",
+  "PWM_CLOCK_DIVIDER_1024",
+  "PWM_CLOCK_DIVIDER_512",
+  "PWM_CLOCK_DIVIDER_256",
+  "PWM_CLOCK_DIVIDER_128",
+  "PWM_CLOCK_DIVIDER_64",
+  "PWM_CLOCK_DIVIDER_32",
+  "PWM_CLOCK_DIVIDER_16",
+  "PWM_CLOCK_DIVIDER_8",
+  "PWM_CLOCK_DIVIDER_4",
+  "PWM_CLOCK_DIVIDER_2",
+  "PWM_CLOCK_DIVIDER_1",
 
   'init'
 ]
